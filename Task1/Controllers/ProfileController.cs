e5dd733f1b935434d52f670a582386e0c702b18e
Nowsher-Ali <80275914+Nowsher-Ali@users.noreply.github.com>
2021-09-30 02:39:48 +0600

    CV

---
+++ Task1/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BIO()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
